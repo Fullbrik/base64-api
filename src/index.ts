bd154adca446b0e72c92c75d3fb3ab1f4ffec9e1
Fullbrik <40151628+Fullbrik@users.noreply.github.com>
2020-12-03 16:34:31 -0800

    Added error handling

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
-import express from 'express';
-import base64 from 'base-64';
-import utf8 from 'utf8';
+import express from "express";
+import base64 from "base-64";
+import utf8 from "utf8";
 
 const port: number = 3000;
 
 const app = express();
 
-app.get('/:base64', (req, res) => {
-    var decoded = base64.decode(req.params.base64);
-    var text = utf8.decode(decoded);
-    res.send(text);
+app.get("/:base64", (req, res) => {
+	var decoded = base64.decode(req.params.base64);
+	var text = utf8.decode(decoded);
+	res.send(text);
+});
+
+app.get("*", (req, res) => {
+    res.send('Please put a bse64 string into url.');
 });
 
 app.listen(port);
 
-console.log("Now listening on port " + port)
+console.log("Now listening on port " + port);
